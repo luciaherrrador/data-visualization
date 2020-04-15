d29e7416677634c022889253a88351de4038e108
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-04-15 19:03:08 +0200

    first commit

---
+++ Bar/README.md
@@ -0,0 +1,4 @@
+# D3 Bar Chart Example
+A basic bar chart in d3.js 
+
+
---
+++ chart/chart.js
@@ -0,0 +1,16 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'radar',
+
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    options: {}
+});
